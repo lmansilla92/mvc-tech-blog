c76072f8aad136e4072063e72d78d53325343fd3
Luis Mansilla <l_mansilla92@yahoo.com>
2023-11-10 07:37:34 -0600

    Added signup handlebar route

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newComment = await Comment.create({
+            ...req.body,
+            user_id: req.session.user_id
+        });
+        res.status(200).json(newComment);
+    } catch (err) {
+        res.status(400).json(err);
+    };
+});
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const commentData = await Comment.findAll({
+            ...req.body,
+            user_id: req.session.user_id
+        });
+        // res.status(200).json(commentData);
+    } catch (err) {
+        res.status(400).json(err);
+    };
+});
+
+
+
+module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -79,4 +79,10 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
+router.get('/signup', (req, res) => {
+
+
+    res.render('signup');
+});
+
 module.exports = router;
