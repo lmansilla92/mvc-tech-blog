2e0818904b8537655a6569c4ed616b30571d3c7f
Luis Mansilla <l_mansilla92@yahoo.com>
2023-11-12 19:48:18 -0600

    Added sign up function

--- public/js/login.js
+++ public/js/login.js
@@ -15,7 +15,7 @@ const loginFormHandler = async (event) => {
 
         if (response.ok) {
             // If successful, redirect the browser to the profile page
-            document.location.replace('/dashboard'); //TODO FIX THIS ROUTE!
+            document.location.replace('/dashboard'); 
             console.log('logged in!');
         } else {
             alert(response.statusText);
