a30075c35b91ae982e1a5082c037522efa7b8088
Luis Mansilla <l_mansilla92@yahoo.com>
2023-11-15 19:59:59 -0600

    Added form to create a comment in blog.handlebars

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,13 +2,16 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', withAuth, async (req, res) => {
+router.post('/:id', withAuth, async (req, res) => {
+    console.log('req.body: ', req.body.description);
     try {
         const newComment = await Comment.create({
             ...req.body,
-            user_id: req.session.user_id
+            user_id: req.session.user_id,
+            blog_id: req.params.id
         });
-        res.status(200).json(newComment);
+        console.log(newComment);
+        res.redirect(`/blogs/${req.params.id}`);
     } catch (err) {
         res.status(400).json(err);
     };
